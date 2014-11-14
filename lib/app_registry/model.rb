6a0f1dfb4f234bb7940f3fc5c656713f166864f8
uno4ki <spam@junki.org>
2014-11-14 10:38:02 +0300

    Beta 1 version bump

--- lib/app_registry/model.rb
+++ lib/app_registry/model.rb
@@ -1,12 +1,9 @@
 require 'active_record'
 
 module AppRegistry
-
   class AppRegistry < ::ActiveRecord::Base
-
     self.primary_key = :name
     validates_presence_of :name, :value
     attr_accessible :name, :value
-
   end
 end
